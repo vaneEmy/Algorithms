ecb2c595f970d0dddf9645320b135034cc8c1432
vaneemy <vanvanemily@gmail.com>
2018-08-25 01:15:21 -0500

    Compare the triples: Refactor code.

--- compare_triplets.rb
+++ compare_triplets.rb
@@ -1,24 +1,15 @@
 # Find the comparison points by comparin a[0] with b[0], a[1] with b[1] and so on.
 
 def compareTriplets(a, b)
-    if a.size > 0 && b.size > 0
-        array_size = a.size
-        count = 0
+    if !a.empty? && !b.empty?
         new_array = []
-        points_from_triplet_one = 0
-        points_from_triplet_two = 0
+        points_from_triplet_one, points_from_triplet_two, count = 0, 0, 0
         
-        while count < array_size
-            if (a[count] > 1 && a[count] < 100) && (b[count] > 1 && b[count] < 100) 
-                if a[count] >  b[count]
-                    points_from_triplet_one += 1
-                elsif a[count] < b[count]
-                    points_from_triplet_two += 1
-                else
-                end
+        if (1..100) === a[count] && (1..100) === b[count] 
+            while count < a.size
+                points_from_triplet_one += 1 if a[count] > b[count]
+                points_from_triplet_two += 1 if a[count] < b[count]
                 count += 1
-                else
-                break
             end
         end
         new_array =  [points_from_triplet_one, points_from_triplet_two]
