ea54d71910a77a7c001039500d9895b6ba537ee9
Vanessa <vanvanemily@gmail.com>
2018-12-26 19:18:20 -0600

    Factorial algorithm in Python

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    # test for a base case
+    if n == 0:
+        return 1
+    #make a calculation and a recursive call
+    else:
+        f= n*factorial(n-1)
+    print(f)
+    return(f)
+
+
+factorial(4)
+
