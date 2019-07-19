ac0d7963d2ce58f50acebb0f1582244135f25acd
Vanessa Emily <vanvanemily@gmail.com>
2019-07-19 15:50:35 -0500

    Factoring code for getting all prime numbers in a specific range

--- get_all_prime_numbers.rb
+++ get_all_prime_numbers.rb
@@ -2,32 +2,28 @@
 
 def all_prime_numbers(number)
     numbers = (1..number)
-    i, prime_numbers = 0, []
+    current_item, prime_numbers = 2, []
     num_max = numbers.size
-     current_item = 2
-    prime_numbers = is_not_prime_number(current_item, numbers)
-    result = []
     
-    while (current_item + 1 < num_max)
-        result = is_not_prime_number(current_item + 1, prime_numbers)
-        prime_numbers = result
-        num_max = result.size
-        current_item+=1
+    while (current_item < num_max)
+        prime_numbers = prime_number(current_item, numbers)
+        numbers = prime_numbers
+        num_max = prime_numbers.size
+        current_item += 1
     end
 
-   
     for i in prime_numbers
         puts i
     end
 end
 
-def is_not_prime_number(current_number, prime_numbers_array)
+def prime_number(current_number, prime_numbers_array)
     prime_numbers_array.select do |num|
         ((num == current_number || num % current_number != 0) && num !=  1)
     end
 end
 
 
-number = 13
+number = 10
 puts "Getting all prime numbers thtat are in the range of 1 to #{number}" 
 all_prime_numbers(number)
