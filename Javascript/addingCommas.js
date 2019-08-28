84d34b61334ea0701b299510ac265e58b67f8be4
Vanessa Emily <vanvanemily@gmail.com>
2019-08-27 22:59:40 -0500

    Adding Commas problem in Javascript.

---
+++ Javascript/addingCommas.js
@@ -0,0 +1,12 @@
+
+function addingCommas(number){
+    let newArr = number.split('');
+
+    for(let i = number.length - 3; i > 0; i-=3){
+        newArr.splice(i, 0, ",");
+    }
+    return newArr.join('');
+}
+
+console.log(addingCommas("100000"));
+
