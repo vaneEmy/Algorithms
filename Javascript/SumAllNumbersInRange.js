1881aa4aee3c44f9c95c1122595ab3fc07382e48
Vanessa Emily <vanvanemily@gmail.com>
2019-08-29 13:24:16 -0500

    Sum all numbers in a range problem in Javaascript.

---
+++ Javascript/SumAllNumbersInRange.js
@@ -0,0 +1,23 @@
+/**
+ * It'll pass you an array of two numbers. 
+ * Return the sum of those two numbers plus the sum of all the numbers between them.
+ * The lowest number will not always come first.
+ * 
+ */
+
+function sumAll(arr) {
+    let sum = 0;
+    let max = arr[1] > arr[0] ? arr[1] : arr[0];
+    let min = arr[1] < arr[0] ? arr[1] : arr[0];
+    
+    for(let i = min; i <= max; i++){
+      sum += i;
+    }
+    
+    return sum;
+  }
+  
+  sumAll([1, 4]);
+  sumAll([4, 1]);
+  sumAll([5, 10]);
+  sumAll([10, 5]);
