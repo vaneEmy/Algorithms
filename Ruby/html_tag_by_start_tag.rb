306fc2528281e4f9d8998883fc8f4ff8ecc58737
Vanessa Emily <vanvanemily@gmail.com>
2019-11-06 13:17:29 -0600

    Hmtl tag by start tag problem solved in Ruby.

---
+++ Ruby/html_tag_by_start_tag.rb
@@ -0,0 +1,17 @@
+# You are implementing your own HTML editor. 
+# To make it more comfortable for developers you would like to add an auto-completion feature to it.
+# Given the starting HTML tag, find the appropriate end tag which your editor should propose.
+
+def htmlEndTagByStartTag(startTag)
+    regExp = /[^<]\w+/
+    tag = regExp.match(startTag)
+    end_tag = "</#{tag}>"
+    #puts "#{end_tag}"
+    end_tag
+end
+
+puts(htmlEndTagByStartTag("<div id='my_area' class='data' title='This is a test for title on Div tag'>")) # Expected_tag: "</div>"
+puts(htmlEndTagByStartTag("<button type='button' disabled>")) # Expected_tag: "</button>"
+puts(htmlEndTagByStartTag("<li class='test'>")) # Expected_tag: "</li>"
+puts(htmlEndTagByStartTag("<TABLE border='1'>")) # Expected_tag: "</TABLE>"
+puts(htmlEndTagByStartTag("<html>")) # Expected_tag: "</html>"
