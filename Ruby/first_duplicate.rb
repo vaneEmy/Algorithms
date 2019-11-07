11d5f1fd35d09af9c57c38f6a4eeb6ceb4161854
Vanessa Emily <vanvanemily@gmail.com>
2019-11-07 08:10:44 -0600

    Refactor code in first duplicate using set in ruby

--- Ruby/first_duplicate.rb
+++ Ruby/first_duplicate.rb
@@ -3,30 +3,28 @@
 # In other words, if there are more than 1 duplicated numbers, 
 # return the number for which the second occurrence has a smaller index than the second occurrence of the other number does.
 # If there are no such elements, return -1.
+require 'set'
 
 def firstDuplicate(a)
-    unique_numbers = []
+    unique_numbers = Set.new 
     
     a.each do |num|
-        if unique_numbers.include?(num)
-            return num
-        else
-            unique_numbers.push(num)
-        end
+        return num if unique_numbers.include?(num)
+        unique_numbers.add(num)
     end
     -1 
 end
 
 
 puts(firstDuplicate([2, 1, 3, 5, 3, 2]))                # Expected output:  3
-#puts(firstDuplicate([2, 2]))                            # Expected output:  2
-#puts(firstDuplicate([2, 4, 3, 5, 1]))                   # Expected output: -1
-#puts(firstDuplicate([1]))                               # Expected output: -1
-#puts(firstDuplicate([5, 5, 5, 5, 5]))                   # Expected output:  5
-#puts(firstDuplicate([2, 1]))                            # Expected output: -1
-#puts(firstDuplicate([2, 1, 3]))                         # Expected output: -1
-#puts(firstDuplicate([2, 3, 3]))                         # Expected output:  3
-#puts(firstDuplicate([3, 3, 3]))                         # Expected output:  3
-#puts(firstDuplicate([8, 4, 6, 2, 6, 4, 7, 9, 5, 8]))    # Expected output:  6
-#puts(firstDuplicate([10, 6, 8, 4, 9, 1, 7, 2, 5, 3]))   # Expected output: -1
-#puts(firstDuplicate([1, 1, 2, 2, 1]))                   # Expected output:  1
+puts(firstDuplicate([2, 2]))                            # Expected output:  2
+puts(firstDuplicate([2, 4, 3, 5, 1]))                   # Expected output: -1
+puts(firstDuplicate([1]))                               # Expected output: -1
+puts(firstDuplicate([5, 5, 5, 5, 5]))                   # Expected output:  5
+puts(firstDuplicate([2, 1]))                            # Expected output: -1
+puts(firstDuplicate([2, 1, 3]))                         # Expected output: -1
+puts(firstDuplicate([2, 3, 3]))                         # Expected output:  3
+puts(firstDuplicate([3, 3, 3]))                         # Expected output:  3
+puts(firstDuplicate([8, 4, 6, 2, 6, 4, 7, 9, 5, 8]))    # Expected output:  6
+puts(firstDuplicate([10, 6, 8, 4, 9, 1, 7, 2, 5, 3]))   # Expected output: -1
+puts(firstDuplicate([1, 1, 2, 2, 1]))                   # Expected output:  1
