1c35bc4e9f4e1d6690b6f1b7543087cfe01008ce
Vanessa Emily <vanvanemily@gmail.com>
2019-11-13 11:20:00 -0600

    Changing name of vars in add border problem.

--- Ruby/add_border.rb
+++ Ruby/add_border.rb
@@ -1,14 +1,14 @@
 # Given a rectangular matrix of characters, add a border of asterisks(*) to it.
 
 def addBorder(picture)
-    border_top_bottom = '*' * (picture.first.size + 2)
+    border = '*' * (picture.first.size + 2)
     
     picture.each do |pic|
         pic.insert(0, '*')
         pic.insert(pic.size, '*')
     end
-    picture.unshift(border_top_bottom)
-    picture.push(border_top_bottom)
+    picture.unshift(border)
+    picture.push(border)
     
     picture
 end
