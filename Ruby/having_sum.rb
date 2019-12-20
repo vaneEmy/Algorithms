e2bda30869e34b300e5754d1e988572966d3da6a
Vanessa Emily <vanvanemily@gmail.com>
2019-12-20 10:26:04 -0600

    Refactor code for having sum problem.

--- Ruby/having_sum.rb
+++ Ruby/having_sum.rb
@@ -3,16 +3,11 @@
 # Given a positive integer n, calculate the following sum: n + n/2 + n/4 + n/8 + .... All divisions are integer.
 
 def halvingSum(n)
-    div = 1
-    sum = (n/div).round
-    res = sum
-
-    while(res != 1)
-        div *= 2
-        res = (n / div).round
-        sum += res
+    sum = n
+    while(n > 1)
+        n = n/2
+        sum += n
     end
-    
     sum
 end
 
