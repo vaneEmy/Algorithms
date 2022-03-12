1fb729c86cb9b8235ca83232e1301fc7060161bd
Vanessa <vanvanemily@gmail.com>
2022-03-11 19:29:55 -0600

    Improve code

--- Python/buying_water.py
+++ Python/buying_water.py
@@ -1,11 +1,9 @@
 def buying_water(brands, prices):
     max_price = prices[0]
-    i = 1
     
-    while i < brands:
+    for i in range(1, brands):
         if prices[i] > max_price: 
             max_price = prices[i]
-        i += 1
     
     return max_price
 
