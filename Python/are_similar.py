0d4d9374754f1e83222d609612e909cc5e0df74d
Vanessa <vanvanemily@gmail.com>
2022-04-07 00:05:15 -0500

    Improve are equal code

--- Python/are_similar.py
+++ Python/are_similar.py
@@ -1,29 +1,43 @@
 ## Solved problem of codesignal: are similar?
 
 def solution(a, b):
-    count_swap = 0
-    i = 0 
-
-    while i < len(a):
-        if a[i] in b:
-            index = b.index(a[i])
-            # If the value is in the same position and are equal
-            if a[i] == b[i]: 
-               i+=1  
-            # If the value is not in the same position, check if swap one element are equal 
-            elif count_swap == 0 and index != i and a[i] == b[index] and b[i] == a[index]:
-                count_swap += 1
-                i = index + 1
-            else: 
-                return False
-           
+    was_swaped = False
+    swaped_numbers = None
+    
+    for ele_a, ele_b in zip(a,b):
+        """
+            si son diferentes:
+                1. es que sea la primera vez -> lo agregamos a swaped_numbers
+                2. es la segunda vez y los numeros coinciden -> was_swaped = true
+                3. es la tercera vez o los numeros no coinciden -> return false
+        """ 
+        are_diferent = ele_a != ele_b
+        swaped_are_equal = (ele_b, ele_a) == swaped_numbers
+        
+        if swaped_numbers is None and are_diferent:
+            swaped_numbers = (ele_a, ele_b)
+        elif (
+            swaped_numbers is not None 
+            and are_diferent
+            and swaped_are_equal
+            and not was_swaped
+        ):
+            was_swaped = True
+        elif (
+            swaped_numbers is not None 
+            and ele_a != ele_b 
+            and (not swaped_are_equal or was_swaped)
+        ):
+            return False
     
     return True
+
 
 test_1 = solution([1, 2, 3],[1, 2, 3]) # True
 test_2 = solution([1, 2, 3],[2, 1, 3]) # True
 test_3 = solution([1, 2, 2],[2, 1, 1]) # False
 test_4 = solution([1, 1, 4],[1, 2, 3]) # False
-test_5 = solution([832, 998, 148, 570, 533, 561, 894, 147, 455, 279],[832, 570, 148, 998, 533, 561, 455, 147, 894, 279]) # False
+test_5 = solution([1, 2, 3],[1, 3, 2]) # True
+test_6 = solution([832, 998, 148, 570, 533, 561, 894, 147, 455, 279],[832, 570, 148, 998, 533, 561, 455, 147, 894, 279]) # False
 
 print(test_5)
