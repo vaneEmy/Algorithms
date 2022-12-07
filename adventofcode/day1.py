44ac903dbc0bc695191409c8569ea940d00d8d1c
Vanessa <vanvanemily@gmail.com>
2022-12-07 01:27:58 -0600

    Add second part

--- adventofcode/day1.py
+++ adventofcode/day1.py
@@ -1,18 +1,32 @@
-def part_one():
+def get_calories():
     with open("input.txt", "r") as f:
         lines = f.readlines()
         calories = []
         sum = 0
         
-        for i in lines:
-            if i.isspace():
+        for line in lines:
+            if line.isspace():
                 calories.append(sum)
                 sum = 0
             else:
-                sum += int(i)
+                sum += int(line)
 
-        return max(calories)
+        return calories
 
+def part_one():
+    calories = get_calories()
+    return max(calories)
 
-result = part_one()
+def part_two():
+    calories = get_calories()
+    sum = 0
+
+    for i in range(3):
+        max_calories = max(calories)
+        sum += max_calories
+        calories.remove(max_calories)
+
+    return sum
+
+result = part_two()
 print(result)
