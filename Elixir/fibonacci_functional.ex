1f8dac1cdf97ee54a554b80327840915e5ea88ee
Vanessa <vanvanemily@gmail.com>
2023-04-07 14:16:23 -0600

    Add fibonacci functional algorithm

---
+++ Elixir/fibonacci_functional.ex
@@ -0,0 +1,21 @@
+defmodule FibonacciFunctional do
+  @moduledoc """
+  The Fibonacci is an infinite sequence of natural numbers in which the two first are 1, 1
+  and the following ones are the sum of the two previous numbers
+
+  1,1,2,3,5,8,13,21,34,55
+  """
+
+  @doc """
+  Given a position it returns the fibonacci number
+  """
+  @spec fib_series(integer()) :: integer()
+  def fib_series(0), do: 0
+  def fib_series(1), do: 1
+  def fib_series(position) do
+    fib_series(position-1) + fib_series(position-2)
+  end
+end
+
+FibonacciFunctional.fib_series(4) # Returns 3
+FibonacciFunctional.fib_series(7) # Returns 13
